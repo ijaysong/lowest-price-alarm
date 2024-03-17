c7d63c4bb1177a8ef2d514b233cac3a3fcbe9a3a
Eunji Song <ijaysong0811@gmail.com>
2024-03-17 15:51:56 +0900

    fix script

---
+++ server/script/initdb/101-dml-basic.sql
@@ -0,0 +1,8 @@
+INSERT INTO company VALUES (1, "W컨셉", 1);
+INSERT INTO company VALUES (2, "29CM", 0);
+INSERT INTO company VALUES (3, "무신사", 0);
+INSERT INTO company VALUES (4, "EQL", 0);
+
+INSERT INTO product_status VALUES (1, "ON-SALE", "판매 중");
+INSERT INTO product_status VALUES (2, "SOLD-OUT", "품절");
+INSERT INTO product_status VALUES (3, "END-OF-SALE", "판매 종료");
